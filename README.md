16ea9e6d44add0dc41a6d9720965f0a6f1d0446e
Troy Conrad <git@troyconrad.com>
2022-01-22 20:32:40 -0800

    Added python setup info to README Added tensorflow to requirements.txt Misc. cosmetic code cleanup (extra spaces, typos, etc.)

--- README.md
+++ README.md
@@ -1,14 +1,25 @@
 # Dogtor AI
- Asimov Dog
+Asimov Dog
 
 
 Download Google drive file 
-
 https://drive.google.com/drive/folders/1Cizpacxh4YzTHVSkSvmpXWVIK3nOJi7b?usp=sharing
+and extract to the main directory under Dogtor-AI so the folder structure looks like:
 
-and extract to the main directory under Dogtor-AI that way the folder structure looks like
+Dogtor-AI
+    audio_files
+    models
 
-1. Dogtor-AI
-     * -audio_files
-    * -models
- 
+
+## Setup
+
+Install libraries in requirements.txt. Use of a virtual env is suggested. On Linux/macOS:
+
+python3 -m venv dogtor-ai-venv
+source dogtor-ai-venv/bin/activate
+pip install -r requirements.txt
+
+
+## Usage
+
+python listen_woof.py # starts the listener
--- listen_woof.py
+++ listen_woof.py
@@ -1,23 +1,28 @@
-#! ~/prog/DogPI/bin/python
+#!~/prog/DogPI/bin/python
+
 # -*- coding: utf-8 -*-
 """
 Created on Thu Oct 28 18:55:38 2021
 @author: Alexsey Gromov
 """
+
+VERSION = '0.0.1'
+
 import os
-import sys
+import queue
 import random
 import subprocess
-import queue
-from threading import Thread
+import sys
 
 import numpy as np
 import sounddevice as sd
 import soundfile as sf
 
+from threading import Thread
 
-#Setting Initiation 
-playback= True # playback dog barking sounds
+
+# Setting Initiation 
+playback = True # playback dog barking sounds
 DOCKER = False # docker tensorflow server does not work on arm65
 plot_show = True #shows plot for every sound that activates prediction function aka a loud sound
 sleep_time = 5 #seconds after the computer barks back, we sleep
@@ -28,19 +33,19 @@ audio_dir = './audio_files/' #directory where the barking sounds are
 CONFIDENCE = .88 #Confidence of the prediciton model for identifying if the sound contains dog bark
 
 #Variable initiation #do not change
-save_name = 0  #used for saving waves files # Not sued currently
-buff = np.array([])  #Saves as global data buffer for predicting. If the bark happends at the end or beggining we ened to createa a window overlap
-audio_buffer = 0    #creates an array from buffer #TODO can be combined with buff variable        
+save_name = 0 #used for saving waves files # Not sued currently
+buff = np.array([]) #Saves as global data buffer for predicting. If the bark happends at the end or beggining we ened to createa a window overlap
+audio_buffer = 0 #creates an array from buffer #TODO can be combined with buff variable
 woof_count = 0 #initialize count for dog barks
 
 
 
+print(f"Dogtor AI version {VERSION} initializing...")
 
-
-#Darwin = mac os x , win32 = windows
+# darwin = macOS, win32 = Windows
 if sys.platform in "darwin win32":
     from playsound import playsound
-    print("Playsound  will be use because this is MAC OR PC")
+    print("macOS or Windows detected, using playsound")
 else:
     from sound_player import SoundPlayer, Sound
 
@@ -52,10 +57,10 @@ def run_tensor():
     tflite_model= r'./models/woof_friend_final.tflite'
     interpreter = tf.lite.Interpreter(tflite_model)
     interpreter.allocate_tensors()
-        
+
     return interpreter
 
-#2 version one for rapsberry one for windows    
+#2 version one for raspberry one for windows
 if sys.platform in "darwin win32":  
     def play_woof():
         #TODO change to sd.play() remove thread?
@@ -67,22 +72,23 @@ else:
         audio_file = random.choice([x for x in os.listdir(audio_dir) if x.endswith(".mp3")] )
         print(audio_dir+audio_file)
         player = SoundPlayer()
-        player.enqueue(Sound(audio_dir+audio_file), 1)    
-        player.play()  
+        player.enqueue(Sound(audio_dir+audio_file), 1)
+        player.play()
 
 
-def mic_index(dev_name  ='yeti'): #get blue yetti mic index
+
+def mic_index(dev_name ='yeti'): #get blue yetti mic index
     devices = sd.query_devices()
     print('index of available devices')
     for i, item in enumerate(devices):
         try:
-            if (dev_name in item['name'].lower() )and ("micro" in item['name'].lower()):
+            if ( dev_name in item['name'].lower() ) and ( 'micro' in item['name'].lower() ):
                 print(i,":", item['name'], "Default SR: ",item['default_samplerate'])
                 sr = int(item['default_samplerate'])
                 return i, sr
         except:
             pass
-    
+
 
 def variance(data):
       data = np.abs(data)
@@ -97,13 +103,13 @@ def variance(data):
       return variance
 
 
-#thie function needs alot of work but x2 loudness works fine for now
+#this function needs alot of work but x2 loudness works fine for now
 def loudness():
     stream = sd.InputStream(channels=1, 
-                            device =  dev_mic,    
+                            device = dev_mic,
                             )
     stream.start()
-    n_sample =  stream.read(int(RATE*1))[0] # reads 4 seconds of scilence
+    n_sample = stream.read(int(RATE*1))[0] # reads 4 seconds of scilence
     stream.stop()
     stream.close()
     noise_sample = n_sample
@@ -111,12 +117,12 @@ def loudness():
     # plotAudio2(noise_sample)
     variance(noise_sample)
     if variance(noise_sample) > .000001:
-        loud_threshold =  np.mean(np.abs(noise_sample))
+        loud_threshold = np.mean(np.abs(noise_sample))
         noise_sample =noise_sample[np.abs(noise_sample)< loud_threshold*1]
-        loud_threshold =  np.max(np.abs(noise_sample))*1
+        loud_threshold = np.max(np.abs(noise_sample))*1
         
     else: 
-        loud_threshold =  np.max(np.abs(noise_sample))*1
+        loud_threshold = np.max(np.abs(noise_sample))*1
     print(variance(noise_sample))
     # plotAudio2(noise_sample)
     print("Loud threshold", loud_threshold)
@@ -138,33 +144,35 @@ def update_plot(frame):
     
 ###############################################################################
 #main callback funtion for the stream : This is done in new thread per sounddevice
-# NOTE: that  woof = 0 needed to set woof prediction to false 
+# NOTE: that woof = 0 needed to set woof prediction to false 
 def callback(indata, frames, time, status, woof= 0):
     global woof_count 
-    global buff   
+    global buff
     global audio_buffer
+    global plot_show
+
     # global model
     if status:
         print(status)
-        
+
     if any(indata):
         print(frames)
         if all(buff) == None:
             buff = np.squeeze(indata)
             print("init_concat")
         else:
-            buff= np.concatenate((buff[-int(RATE*BUFFER_ADD):] ,np.squeeze(indata)))
+            buff = np.concatenate((buff[-int(RATE*BUFFER_ADD):] ,np.squeeze(indata)))
             if(np.mean(np.abs(indata))<loud_threshold):
                 pass
-                #print("inside silence reign")   
+                #print("inside silence reign")
             else:
                 audio_buffer = buff[np.newaxis,:]
                 #print("length audio buffer", audio_buffer.shape)
                 global data
                 woof, array, data = predict(audio_buffer, interpreter, confidence=.93, wording = True)
 
-                if plot_show ==  True:
-                    p.put(data)                   
+                if plot_show == True:
+                    p.put(data)
             if woof == 1:
                 print("woof woof a dog was heard")
                 woof_count+=1
@@ -177,7 +185,7 @@ def callback(indata, frames, time, status, woof= 0):
                     print(f'sleeping for {sleep_time} seconds')
                     sd.sleep(int(sleep_time*1000)) # put the sound stream to sleep
                     music_thread.join()
-                
+
                 #save file to desctop to analize
                 # wf = wave.open('./save_audio/Aduio_clip_'+str(save_name)+".wav", 'wb')
                 # t = np.linspace(0., 1., samplerate)
@@ -186,16 +194,16 @@ def callback(indata, frames, time, status, woof= 0):
                 # write('./save_audio/Aduio_clip_'+str(save_name)+".wav", RATE, audio_buffer)
                 # print("File/Audio saved")
                 # save_name +=1
-                
+
     else:
-        print('no input')    
+        print('no input')
 
 
 #####################################################################################
 
 
 
-# dev_mic, RATE =  mic_index()
+# dev_mic, RATE = mic_index()
 #Detect Loudness minimum level. When loudness exceeds threshold detection for dog barks is triggered
 
 
@@ -204,28 +212,30 @@ devices = sd.query_devices()
 print(devices)
 dev_mic = int(input("Enter mic number to use: "))
 
- #Rate of the microphone is overwritten later
-RATE =  22050   # samnples per second : Setting custom rate to 22050 instead of 44100 to save on computational time
+#Rate of the microphone is overwritten later
+RATE = 22050 # samples per second : Setting custom rate to 22050 instead of 44100 to save on computational time
 loud_threshold =  loudness()
 # model = import_model()
 #Loop Start #################################################################################################
+
+if True:
+    import matplotlib.pyplot as plt #cannot be ran on raspberry pi for now
+    from matplotlib.animation import FuncAnimation
+    p = queue.Queue()
+    plotdata = []
+    fig = plt.figure()
+    #initialize plot
+    plotdata.append(np.random.rand(40,87)) # set initial value for the plot to get a frame size etc. This locks the plot in place for future
+    im =plt.imshow(plotdata[0], animated = True)
+    fig.tight_layout(pad=0)
+
 try:
-    if plot_show == True:
-        import matplotlib.pyplot as plt #cannot be ran on raspberry pi for now
-        from matplotlib.animation import FuncAnimation
-        p = queue.Queue()
-        plotdata = []
-        fig = plt.figure()
-        #initialize plot
-        plotdata.append(np.random.rand(40,87)) # set initial value for the plot to get a frame size etc. This locks the plot in place for future
-        im =plt.imshow(plotdata[0], animated = True)
-        fig.tight_layout(pad=0)
     print("loading model")
     interpreter = run_tensor()
     print("loadinsound input")
 
-    stream =  sd.InputStream(device = int(dev_mic),
-                        channels =  1,
+    stream = sd.InputStream(device = int(dev_mic),
+                        channels = 1,
                         samplerate = RATE,
                         callback=callback,
                         blocksize=int(RATE*BUFFER_SECONDS),
@@ -234,12 +244,12 @@ try:
     #Sets plot to update automatically on interval of 2?? what ever that is blit False is the only way it works with the update_plot function not having a return variable at the same time. 
     if plot_show == True:
         ani = FuncAnimation(fig, update_plot, interval=2 , blit=False)
-        
+
     with stream:
         while True:
             if plot_show == True:
                 plt.show()
-            
+
             response = input()
             if response in ('', 'q', 'Q'):
                 stop_docker()
@@ -255,11 +265,8 @@ try:
             #         break
 
 except Exception as e:
-    #subprocess.Popen('docker stop tensor')  
-    print("end",  e)
-    
-    
-    
+    #subprocess.Popen('docker stop tensor')
+    print("end", e)
 
 
 
